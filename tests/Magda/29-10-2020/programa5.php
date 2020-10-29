92a9e33bc9e916bbe720f4fcd6ba3c08e0dcda67
albamagda <albamagdaleno@gmail.com>
2020-10-29 20:34:07 +0100

    nuevas practicas

---
+++ tests/Magda/29-10-2020/programa5.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  $dia = 5; //Se declara una variable de tipo integer.
+  $temp = 36.7; //Se declara una variable de tipo double.
+  $ciudad = "Valencia"; //Se declara una variable de tipo string.
+  $exite = true; //Se declara una variable boolean.
+  echo "Variable entera:";
+  echo $dia;
+  echo "<br>";
+  echo "Variable double:";
+  echo $temp;
+  echo "<br>";
+  echo "Variable string:";
+  echo $ciudad;
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $exite;
+  ?>
+</body>
+
+</html>
---
+++ tests/Magda/29-10-2020/programa6.php
@@ -0,0 +1,20 @@
+<html>
+
+    <head>
+        <title>Problema</title>
+    </head>
+
+    <body>
+
+        <?php
+           $var1 = "vainilla";
+           $var2 = "chocolate";
+           $var3 = "yogur";
+           $gelato = $var1 ." ". $var2 ." ". $var3 . "<br><br>";
+           echo $gelato ;
+           $num = 25;
+           echo "De nuestros $num sabores, el más solicitado es el de $var3";
+        ?>
+    </body>
+
+</html>
