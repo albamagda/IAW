0a89c8ba02d72a4aeea9b412a0c71dd5500e4d5d
albamagda <albamagdaleno@gmail.com>
2020-10-29 20:58:38 +0100

    sinacabar

---
+++ tests/Magda/29-10-2020/programa8.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+    <?php
+        for ($f = 2; $f <= 20; $f = $f + 2) {
+            echo $f;
+            echo "<br>";
+        }
+    ?>
+</body>
+
+</html>
