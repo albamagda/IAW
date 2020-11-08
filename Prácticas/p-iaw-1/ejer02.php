f625fe8b6849c8c830e4e3af98bf4179f1ab275e
Alba Magdaleno <albamagdaleno@MacBook-Pro-de-Alba.local>
2020-11-08 17:50:48 +0100

    -

--- Prácticas/p-iaw-1/ejer02.php
+++ Prácticas/p-iaw-1/ejer02.php
@@ -1,9 +1,22 @@
-<?php
-    $resultado = $uno x $mult
+<html>
+    <head>
+        <title> Ejercicio2 </title>
+    </head>
+    <body>
+        <form>
+            <p>Inserte un valor: <input type="text" name="num"> </p>
+        </form>
+
+        <?php
+            print "<p> El número es $_REQUEST[num]</p>";
+            $numero=$_REQUEST['num'];
+
+            for ($i=1; $i<=10; $i++) {
+                echo $numero ."*". $i ."=". $numero*$i ."<br>" ;
+            }
+        ?>
+    </body>
+</html>
+
+
     
-    if [$resultado < ($uno x 10)]{
-        $mult = $mult + 1 ;
-    } else{
-        
-    }
-?>
