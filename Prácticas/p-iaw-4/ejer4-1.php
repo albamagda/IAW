85e4fcfea5ef5d00fb31c86b224ebf70fd0da6df
Alba Magdaleno <albamagdaleno@MacBook-Pro-de-Alba.local>
2020-11-12 18:57:33 +0100

    p4

---
+++ Prácticas/p-iaw-4/ejer4-1.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title> Ejercicio 4_1 </title>
+    </head>
+    <body>
+       <?php 
+            $a="";
+            $b="";
+            function getGratherValue($a,$b){
+                if($a > $b){
+                    echo $a." es mayor que ".$b;
+                }else{
+                    echo $b." es mayor que ".$a;
+                }
+            }
+       ?> 
+    </body>
+</html>
