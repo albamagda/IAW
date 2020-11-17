0eb5cb0a27e0dbe5a416361a0894126b1c7a78f9
albamagda <albamagdaleno@gmail.com>
2020-11-17 20:43:54 +0100

    inicial

---
+++ Prácticas/p-iaw-5/ejerfor.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> Práctica 5 // FOR </title>
+    </head>
+    <body>
+        <?php
+            $email="alba.magrec@educa.jcyl.es";
+
+            for($i = 0; $i <= 10; $i++){
+                echo $email;
+            }
+        ?>
+    </body>
+</html>
