4a21ff4813ad032846bdec46a157326a2e2a8e4d
albamagda <albamagdaleno@gmail.com>
2020-11-19 20:14:17 +0100

    -

--- Prácticas/p-iaw-6/database.php
+++ Prácticas/p-iaw-6/database.php
@@ -1,26 +1,35 @@
-  
-<?php
-//https://www.hostinger.es/tutoriales/conectar-php-mysql/
+<html>
+    <head>
+        <title>Práctica 6</title>
+    </head>
+    <body>
+        <h1>DB World</h1>
+        <ol>
+            <?php 
+                
+                $servername = "localhost";
+                $database = "world";
+                $username = "test";
+                $password = "test";
 
-$servername = "localhost";
-$database = "world";
-$username = "test";
-$password = "test";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-$query = "SELECT Name, CountryCode FROM City ORDER by ID LIMIT 33";
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-if ($resultado = $conn->query($query)) {
-    while( $row = $resultado->fetch_array() )
-    {
-        echo $row['Name'] . " " . $row['CountryCode'];
-        echo "<br />";
-    }
-    /* liberar el conjunto de resultados */
-    $resultado->close();
-}
-echo "Connected successfully";
-mysqli_close($conn);
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $database);
+                $query = "SELECT * FROM city LIMIT 10";
+                // Check connection
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+                if ($resultado = $conn->query($query)) {
+                    while( $row = $resultado->fetch_array() )
+                    {
+                        echo "<li>". $row['Name'] . " pertenece al distrito " . $row['District']. "</li>";
+                        echo "<br />";
+                    }
+                    $resultado->close();
+                }
+                echo "Connected successfully";
+                mysqli_close($conn);
+            ?>
+        </ol>
+    </body>
+</html>
---
+++ Prácticas/p-iaw-6/florindo.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Práctica 6</title>
+    </head>
+    <body>
+        <h1>DB World</h1>
+        <?php
+        //https://www.hostinger.es/tutoriales/conectar-php-mysql/
+
+        $servername = "localhost";
+        $database = "world";
+        $username = "test";
+        $password = "test";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        $query = "SELECT Name, CountryCode FROM City ORDER by ID LIMIT 33";
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        if ($resultado = $conn->query($query)) {
+            while( $row = $resultado->fetch_array() )
+            {
+                echo $row['Name'] . " " . $row['CountryCode'];
+                echo "<br />";
+            }
+            /* liberar el conjunto de resultados */
+            $resultado->close();
+        }
+        echo "Connected successfully";
+        mysqli_close($conn);
+        ?>
+    </body>
+</html>
