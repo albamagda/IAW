8ab7122b640868df2c4b2e5581d365c1d51ea16a
albamagda <albamagdaleno@gmail.com>
2020-12-03 19:09:12 +0100

    -

--- Practicas/practica-php/carta-alta.php
+++ Practicas/practica-php/carta-alta.php
@@ -24,14 +24,36 @@
 
 <body>
   <h1>La carta más alta</h1>
-
+  
   <p>Actualice la página para mostrar un nuevo trío de cartas.</p>
+    <?php
+      $num1 = rand(1,10);
+      $num2 = rand(1,10);
+      $num3 = rand(1,10);
 
-<?php
-
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
-
-?>
+      $imgsrc1 = "img/c".$num1.".svg";
+      $imgsrc2 = "img/c".$num2.".svg";
+      $imgsrc3 = "img/c".$num3.".svg";
+      
+      
+        echo  "<img  src=".$imgsrc1.">";
+        echo  "<img  src=".$imgsrc2.">";
+        echo  "<img  src=".$imgsrc3."> <br>";
+    ?>
+    <?php
+      $carta_alta=$num1;
+    
+      if($num2 > $carta_alta){
+        $carta_alta=$num2;
+      }
+      if ($num3 > $carta_alta){
+        $carta_alta=$num3;
+      }
+      
+      $imgsrcalta = "img/c".$carta_alta.".svg";
+      echo "La carta más alta es <img  src=".$imgsrcalta.">";
+      
+    ?>
 
   <footer>
     <p>Escriba aquí su nombre</p>
