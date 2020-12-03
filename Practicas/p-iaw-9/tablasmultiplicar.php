cd48ffb4e2b2f1033cd1c3eed23e0fbfd170d687
albamagda <albamagdaleno@gmail.com>
2020-12-03 18:35:14 +0100

    preex

---
+++ Practicas/p-iaw-9/tablasmultiplicar.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Ejercicio 1</title>
+    </head>
+    <body>
+        <form action="formulario.php" method="POST" enctype="multipart/form-data">
+            Introduzca el 1er número: <input type='number' name='input_a'><br><br>
+            Introduzca el 2o número: <input type='number' name='input_b'><br><br>
+        </form>
+    </body>
+</html>
