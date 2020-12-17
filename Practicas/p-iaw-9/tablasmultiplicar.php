333a3b916fddece59d9f6a602621d6ae2b1a1c92
albamagda <albamagdaleno@gmail.com>
2020-12-17 19:14:17 +0100

    12/17

--- Practicas/p-iaw-9/tablasmultiplicar.php
+++ Practicas/p-iaw-9/tablasmultiplicar.php
@@ -6,6 +6,7 @@
         <form action="formulario.php" method="POST" enctype="multipart/form-data">
             Introduzca el 1er número: <input type='number' name='input_a'><br><br>
             Introduzca el 2o número: <input type='number' name='input_b'><br><br>
+            <input type='submit' name='enviar' value='enviar'><br><br>
         </form>
     </body>
 </html>
